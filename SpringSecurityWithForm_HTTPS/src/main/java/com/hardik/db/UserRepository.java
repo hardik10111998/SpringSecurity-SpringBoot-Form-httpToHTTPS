2941966ac7c4f130f39c34c2dda0b13115717252
HARDIK <HARDIK@HARDIK>
2020-05-11 17:29:46 +0530

    First Commit

---
+++ SpringSecurityWithForm_HTTPS/src/main/java/com/hardik/db/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hardik.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hardik.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
